77ea9a0acaca675a5e4370b0bc5de1f018182f52
AnaKarolyne <100178048+AnaKarolyne@users.noreply.github.com>
2023-08-27 05:22:20 -0300

    Criação do Projeto
    Missão: Criação do Projeto
    Descrição: Criação da estrutura inicial e programação das primeiras páginas.
    Prazo: 21/08

---
+++ TDSaudeWEB/src/app/historico/page.js
@@ -0,0 +1,14 @@
+import NavBar from "@/components/NavBar";
+
+export default function Historico() {
+  return (
+    <>
+      <NavBar active={"historico"} />
+
+      <main className="bg-slate-900 m-20 p-12 rounded-xl">
+        <h2>Histórico</h2>
+      </main>
+    </>
+
+  )
+}
