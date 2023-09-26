3a8cecceb65db3037d305e10cfaf138b1d4a97c9
AnaKarolyne <100178048+AnaKarolyne@users.noreply.github.com>
2023-09-26 19:52:11 -0300

    Delete TDSaudeWEB/TDSaudeWEB directory

--- TDSaudeWEB/TDSaudeWEB/src/components/InputText.jsx
+++
@@ -1,8 +0,0 @@
-export default function InputText({label, id, name, register=()=>{}, ...props}){
-  return (
-      <div className="flex flex-col gap-1">
-          <label htmlFor={id} className="text-slate-300">{label}</label>
-          <input {...register(name)} type="text" id={id} {...props} className="bg-slate-800 rounded p-2 outline-none focus:ring-1 focus:ring-pink-600" />
-      </div>
-  )
-}
