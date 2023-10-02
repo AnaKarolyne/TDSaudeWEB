b991397ef5a1cb91d3b7f612c453b46b26efb999
AnaKarolyne <100178048+AnaKarolyne@users.noreply.github.com>
2023-10-02 01:32:39 -0300

    Update page.js

--- TDSaudeWEB/src/app/informacoes/page.js
+++ TDSaudeWEB/src/app/informacoes/page.js
@@ -1,15 +1,28 @@
 import NavBar from "@/components/NavBar";
 import DataRow from "./DataRow";
 
-async function getInformacoes(){
-  const url = "http://localhost:8080/api/informacoes"
-  const resp = await fetch(url, { next: { revalidate: 0 } })
-  if (!resp.ok) throw new Error("Não pode carregar os dados")
-  return resp.json()
+async function getInformacoes() {
+  const url = "http://localhost:8080/api/informacoes";
+  const resp = await fetch(url, { next: { revalidate: 0 } });
+  if (!resp.ok) throw new Error("Não pode carregar os dados");
+  return resp.json();
 }
 
 export default function Informacoes() {
-  const data = await geInformacoes()
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const informacoesData = await getInformacoes();
+        setData(informacoesData);
+      } catch (error) {
+        console.error("Erro ao buscar os dados das informações:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -18,10 +31,11 @@ export default function Informacoes() {
       <main className="bg-slate-900 m-20 p-12 rounded-xl">
         <h2 className="text-2xl font-bold">Informações</h2>
         <div id="data" className="text-slate-300 m-1">
-          {data.map(informacoes => <DataRow informacoes={informacoes} /> )}
+          {data.map((informacoes, index) => (
+            <DataRow key={index} informacoes={informacoes} />
+          ))}
         </div>
       </main>
     </>
-
-  )
+  );
 }
