3d4083335a8a76f46d85e1f8840b37c2576164ba
AnaKarolyne <100178048+AnaKarolyne@users.noreply.github.com>
2023-10-02 01:27:27 -0300

    Update page.js

--- TDSaudeWEB/src/app/emergencias/page.js
+++ TDSaudeWEB/src/app/emergencias/page.js
@@ -1,33 +1,42 @@
 import NavBar from "@/components/NavBar";
 import DataRow from "./DataRow";
+import Button from "@/components/Button";
+import { getEmergencias } from "@/actions/emergencias";
 
-async function getEmergencias(){
-  const url = "http://localhost:8080/api/emergencias"
-  const resp = await fetch(url, { next: { revalidate: 0 } })
-  if (!resp.ok) throw new Error("Não pode carregar os dados")
-  return resp.json()
-}
+export default function Emergencia() {
+  const [data, setData] = useState([]);
 
-export default function Emergencias() {
-  const data = await getEmergencias()
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const consultaData = await getEmergencias();
+        setData(consultaData);
+      } catch (error) {
+        console.error("Erro ao buscar os dados de emergência:", error);
+      }
+    }
+
+    fetchData();
+  }, []); // 
 
   return (
     <>
-      <NavBar active={"emergencias"} />
+      <NavBar active={"emergencia"} />
 
       <main className="bg-slate-900 m-20 p-12 rounded-xl">
         <div className="flex justify-between items-center">
-          <h2 className="text-2xl font-bold">Emergencias</h2>
+          <h2 className="text-2xl font-bold">Emergência</h2>
           <Button href="/emergencias/new">
             Registrar Nova Informação de Emergência
           </Button>
         </div>
 
         <div id="data" className="text-slate-300 m-1">
-          {data.map(emergencias => <DataRow emergencias={emergencias} /> )}
+          {data.map((emergencia, index) => (
+            <DataRow key={index} emergencia={emergencia} />
+          ))}
         </div>
       </main>
     </>
-
-  )
+  );
 }
